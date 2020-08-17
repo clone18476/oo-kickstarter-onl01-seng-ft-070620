e3397c970cf96c71defa2a3fc306c88552be1809
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-17 20:03:42 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,17 +1,31 @@
 
+#class Backer 
+#  attr_accessor :backed_projects, :backers
+#  attr_reader :name     # name accessible through an attribute reader
+#  
+#  def initialize(name)    # takes a name on initialization, accessible through an attribute reader
+#    @name = name
+#    @backed_projects = []    # initializes with a @backed_projects attribute, an empty array
+#  end
+#  
+#  def back_project(project)           # accepts a Project as an argument and stores the project
+#    @backed_projects << project     # argument in a backed_projects array
+#    project.backers << self         
+#  end
+#end
+
 class Backer 
   attr_accessor :backed_projects, :backers
-  attr_reader :name     # name accessible through an attribute reader
+  attr_reader :name
   
-  def initialize(name)    # takes a name on initialization, accessible through an attribute reader
+  def initialize(name)
     @name = name
-    @backed_projects = []    # initializes with a @backed_projects attribute, an empty array
+    @backed_projects = []
   end
   
-  def back_project(project)           # accepts a Project as an argument and stores the project
-    @backed_projects << project     # argument in a backed_projects array
-    project.backers << self         
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
   end
 end
-
 
